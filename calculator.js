efcdaf6cb2b50b6c929719c27af59c2b86896e0b
Konovalov Venia <veniakonovalov2001@gmail.com>
2017-06-16 20:37:56 +0300

    consoleCalculator

--- calculator.js
+++ calculator.js
@@ -1,4 +1,7 @@
-﻿function addition (2,2) {
-   var result = a+b
-   console.log(result);
+﻿function addition(a,b) {
+   var a = 2;
+   var b = 2;
+   var result = a+b;
+   
+   console.log(result)
 }
